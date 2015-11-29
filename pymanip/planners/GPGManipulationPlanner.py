bdfd5e8089c50e3273fcee202fee81205f00bc88
Puttichai <L.Puttichai@gmail.com>
2015-11-29 11:42:02 +0800

    added license

--- pymanip/planners/GPGManipulationPlanner.py
+++ pymanip/planners/GPGManipulationPlanner.py
@@ -1,3 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2015 Puttichai Lertkultanon <L.Puttichai@gmail.com>
+#
+# This file is part of pymanip.
+#
+# pymanip is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# pymanip is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# pymanip. If not, see <http://www.gnu.org/licenses/>.
+
 import openravepy as orpy
 import numpy as np
 import time
@@ -1025,7 +1045,7 @@ class GPGManipulationPlanner(bmp.BaseManipulationPlanner):
 
             if not self.CheckGrasp(sol, Tobj):
                 continue
-
+            
             passed = True
             break
         
