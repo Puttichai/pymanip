2568134d2f177cd4e7792bcd5c0335356b4c6edd
Puttichai <L.Puttichai@gmail.com>
2016-03-31 12:51:25 +0800

    now creating ~/.pymanip during setup to store various (data) files

--- setup.py
+++ setup.py
@@ -1,7 +1,19 @@
 from setuptools import setup
+import os
 
 setup(name='pymanip',
       packages=['pymanip', 
                 'pymanip.planners', 
                 'pymanip.utils'],
       include_package_data=True)
+
+# Create a pymanip directory to store various files
+home = os.path.expanduser('~')
+pymanipdir = os.path.join(home, '.pymanip')
+if not os.path.exists(pymanipdir):
+    os.makedirs(pymanipdir)
+
+# Change permission to pymanip directory
+uid = int(os.environ.get('SUDO_UID'))
+gid = int(os.environ.get('SUDO_GID'))
+os.chown(pymanipdir, uid, gid)
